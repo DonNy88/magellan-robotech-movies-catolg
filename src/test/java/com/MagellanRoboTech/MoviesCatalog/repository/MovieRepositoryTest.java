8e5fe72513db42b8c58224f4a197b9798bc3d011
Donald Emeka Achugo <achugo.donald@gmail.com>
2020-07-14 07:40:51 +0200

    configure MovieRepository test

---
+++ src/test/java/com/MagellanRoboTech/MoviesCatalog/repository/MovieRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.MagellanRoboTech.MoviesCatalog.repository;
+
+import com.MagellanRoboTech.MoviesCatalog.MoviesCatalogApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MoviesCatalogApplication.class)
+public class MovieRepositoryTest {
+}
