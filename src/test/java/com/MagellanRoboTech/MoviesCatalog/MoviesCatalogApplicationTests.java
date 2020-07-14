2d2224c095bcb9c3626cd589689c7085acd46362
Donald Emeka Achugo <achugo.donald@gmail.com>
2020-07-14 13:16:51 +0200

    added findAllByMovieDirectorId test

--- src/test/java/com/MagellanRoboTech/MoviesCatalog/MoviesCatalogApplicationTests.java
+++ src/test/java/com/MagellanRoboTech/MoviesCatalog/MoviesCatalogApplicationTests.java
@@ -10,7 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class MoviesCatalogApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	public void contextLoads() { }
+
+
 
 }
--- src/test/java/com/MagellanRoboTech/MoviesCatalog/repository/MovieRepositoryTests.java
+++ src/test/java/com/MagellanRoboTech/MoviesCatalog/repository/MovieRepositoryTests.java
@@ -3,6 +3,7 @@ package com.MagellanRoboTech.MoviesCatalog.repository;
 import com.MagellanRoboTech.MoviesCatalog.model.Movie;
 import com.MagellanRoboTech.MoviesCatalog.model.MovieDirector;
 import org.apache.commons.collections4.IterableUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -21,45 +22,77 @@ public class MovieRepositoryTests {
     @Autowired
     private MovieRepository movieRepository;
 
+    @Autowired
+    private  MovieDirectorRepository movieDirectorRepository;
+
+    @Test
+    public void findAllByRatingGreaterThanEqual_shouldReturnMovieWithRatingGreaterThanOrEqualTo3() {
+
+        List<Movie> moviesAbove3Rating = IterableUtils.toList(movieRepository.findAllByRatingGreaterThanEqual(3));
+
+        assertThat(moviesAbove3Rating).isNotEmpty();
+        moviesAbove3Rating.forEach((elem) -> {
+            assertThat(elem.getRating()).isGreaterThanOrEqualTo(3);
+        });
+    }
+
+    @Test
+    public void findAllByRatingGreaterThanEqual_shouldReturnNoMovies() {
+
+        List<Movie> moviesAbove3Rating = IterableUtils.toList(movieRepository.findAllByRatingGreaterThanEqual(5));
+
+        assertThat(moviesAbove3Rating).isEmpty();
+    }
+
+    @Test
+    public void findAllByMovieDirectorId_shouldReturnANotEmptyListOfMovies() {
+        Long id = IterableUtils.toList(movieDirectorRepository.findAll()).get(0).getId();
+
+        List<Movie> movies = IterableUtils.toList(movieRepository.findAllByMovieDirectorId(id));
+
+        assertThat(movies).isNotEmpty();
+    }
+
     @BeforeEach
     public void emptyDB() {
         movieRepository.deleteAll();
+        movieDirectorRepository.deleteAll();
 
         MovieDirector movieDirector = new MovieDirector();
         movieDirector.setSurname("Test Surname");
         movieDirector.setName("Test Name");
         movieDirector.setMiddleName("Test MiddleName");
+
         Movie movie = new Movie();
         movie.setTitle("Test title 1");
         movie.setMovieDirector(movieDirector);
         movie.setDuration(300L);
         movie.setOverview("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-        movie.setRating(3);
+        movie.setRating(1);
         movieRepository.save(movie);
+
         movie = new Movie();
         movie.setTitle("Test title 2");
         movie.setMovieDirector(movieDirector);
         movie.setDuration(300L);
         movie.setOverview("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+        movie.setRating(2);
+        movieRepository.save(movie);
+
+        movie = new Movie();
+        movie.setTitle("Test title 3");
+        movie.setMovieDirector(movieDirector);
+        movie.setDuration(300L);
+        movie.setOverview("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+        movie.setRating(3);
+        movieRepository.save(movie);
+
+        movie = new Movie();
+        movie.setTitle("Test title 4");
+        movie.setMovieDirector(movieDirector);
+        movie.setDuration(300L);
+        movie.setOverview("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
         movie.setRating(4);
         movieRepository.save(movie);
-    }
-
-    @Test
-    public void findAllByRatingGreaterThanEqual_whenSaveMoviesWithMoreThan2RatingSaved_ExpectedMovieSaved() {
-
-        List<Movie> moviesAbove3Rating = IterableUtils.toList(movieRepository.findAllByRatingGreaterThanEqual(3));
-
-        assertThat(moviesAbove3Rating).isNotEmpty();
-        assertThat(moviesAbove3Rating.get(0).getTitle()).containsPattern("^Test title.+");
-        assertThat(moviesAbove3Rating.get(1).getTitle()).containsPattern("^Test title.+");
-    }
-
-    @Test
-    public void findAllByRatingGreaterThanEqual_whenMovieWithMoreThan2RatingSaved_ExpectedNoMovies() {
-
-        List<Movie> moviesAbove3Rating = IterableUtils.toList(movieRepository.findAllByRatingGreaterThanEqual(5));
-
-        assertThat(moviesAbove3Rating).isEmpty();
     }
 }
