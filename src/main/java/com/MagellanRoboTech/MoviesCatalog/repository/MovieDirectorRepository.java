c9256f8dc9111950d0688300f2c2d0b4fb8c3e06
Donald Emeka Achugo <achugo.donald@gmail.com>
2020-07-15 01:05:41 +0200

    added java doc on all repositories classes

--- src/main/java/com/MagellanRoboTech/MoviesCatalog/repository/MovieDirectorRepository.java
+++ src/main/java/com/MagellanRoboTech/MoviesCatalog/repository/MovieDirectorRepository.java
@@ -4,5 +4,10 @@ import com.MagellanRoboTech.MoviesCatalog.model.MovieDirector;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface that performs all Movie Director queries.
+ *
+ * @see org.springframework.data.repository.Repository
+ */
 @Repository
 public interface MovieDirectorRepository extends CrudRepository<MovieDirector, Long> { }
--- src/main/java/com/MagellanRoboTech/MoviesCatalog/repository/MovieRepository.java
+++ src/main/java/com/MagellanRoboTech/MoviesCatalog/repository/MovieRepository.java
@@ -4,20 +4,25 @@ import com.MagellanRoboTech.MoviesCatalog.model.Movie;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface that performs all Movie queries.
+ *
+ * @see org.springframework.data.repository.Repository
+ */
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Long> {
 
     /**
-     *
-     * @param rating
-     * @return
+     * Fetch all movies with rating greater than or Equal to the rating provided.
+     * @param rating rating for filter
+     * @return the result of the query
      */
     Iterable<Movie> findAllByRatingGreaterThanEqual(Integer rating);
 
     /**
-     *
-     * @param movieDirectorId
-     * @return
+     * Search all movies by movie Director Id.
+     * @param movieDirectorId the movie Director Id for filter.
+     * @return the result of the query
      */
     Iterable<Movie> findAllByMovieDirectorId(Long movieDirectorId);
 }
